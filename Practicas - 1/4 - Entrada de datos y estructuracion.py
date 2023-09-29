d91fb848aafce59aaa0c2bfadc9972686ac5f022
dan260gk <90198305+dan260gk@users.noreply.github.com>
2023-09-28 23:35:25 -0500

    finalizado

--- Practicas - 1/4 - Entrada de datos y estructuracion.py
+++ Practicas - 1/4 - Entrada de datos y estructuracion.py
@@ -3,10 +3,18 @@
 # “{asignatura}” tiene “{créditos}” créditos. Y la suma de todos los créditos del semestre y una LISTA de 
 # todas las materias 
 x="0"
-reticula={}
+semestre={}
+totalCreditos=0
+materias=[]
 while x=="0":
-    s=input("nombre de la asignatura")
-    reticula[s]=input("creditos:")
-    x = input("ingrese 0 para agregar otra asingatura o 1 para guardar los datos")
-print(reticula)
+    s=input("\nnombre de la asignatura: ")
+    semestre[s]=input("creditos: ")
+    x = input("ingrese 0 para agregar otra asingatura o 1 para guardar los datos: ")
+for asignatura, creditos in semestre.items():
+    print(f'"{asignatura}" tiene "{creditos}" créditos')
+    totalCreditos = totalCreditos+int(creditos)
+    materias.append(asignatura)
+
+print(f"\nTotal de créditos del semestre: {totalCreditos}")
+print(f"lista de materias: {materias}")
 
